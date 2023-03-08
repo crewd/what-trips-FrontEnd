7fbfe3b6478b47f32d6d3e968da07c3585e50762
charade6 <charade6@naver.com>
2023-03-08 15:40:31 +0900

    feat: 회원가입 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./components/app-layout";
 import Login from "./pages/login";
+import SignUp from "./pages/signup";
 
 function App() {
   return (
     <BrowserRouter>
       <AppLayout>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </AppLayout>
     </BrowserRouter>
