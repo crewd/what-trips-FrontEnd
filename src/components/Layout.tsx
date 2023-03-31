f3f85e1bac95cb202d882efc46d27c67c9a54724
charade6 <charade6@naver.com>
2023-03-31 21:39:28 +0900

    feat: 지도 view

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./Header";
 
 export const AppLayout: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
     <>
       <Header />
       <main className="w-full pt-[56px] md:pt-[64px]">
-        <div className="max-w-5xl p-4 m-auto">
+        {pathname.slice(-4) !== "/map" ? (
+          <div className="max-w-5xl p-4 m-auto">
+            <Outlet />
+          </div>
+        ) : (
           <Outlet />
-        </div>
+        )}
       </main>
     </>
   );
